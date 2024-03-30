f24c089960fd4422d1b02bd30d346a4ae4db897c
elanajaja <elanjaja01@gmail.com>
2024-03-29 17:48:12 -0700

    binary trees

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,62 +1,81 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_perfect - this function test if a binary tree is prefect
- * @tree: pointer to the root node of the tree to check
- * @d: depth of the tree
- * Return: 1 or 0
- */
-
-int  binary_tree_perfect(const binary_tree_t *tree, int d)
-{
-	if (tree == NULL)
-		return (1);
-
-	if (tree->left == NULL && tree->right == NULL)
-		return (d == 0);
-
-	if (tree->left == NULL || tree->right == NULL)
-		return (0);
-
-	return (binary_tree_perfect(tree->left, d -  1) &&
-			binary_tree_perfect(tree->right, d - 1));
-}
-
-/**
- * binary_tree_depth - measures the depth of a binary tree
- * @tree: pointer to the root node of the tree to measure the height
- * Return: the depth or 0
- */
-
-size_t binary_tree_depth(const binary_tree_t *tree)
-{
-	size_t depth;
-
-	if (tree == NULL)
-		return (0);
-
-	depth = binary_tree_depth(tree->parent);
-
-	if (tree->parent != NULL)
-		return (depth + 1);
-
-	return (0);
-}
-
-/**
  * binary_tree_is_perfect - checks if a binary tree is perfect
- * @tree: pointer to the root node of the tree to check
- * Return: 1 or 0
+ * @tree: a pointer to the root node of the tree to check
+ *
+ * Return: 1 if the tree is perfect
+ *         0 if the tree is not perfect
+ *         0 if tree is NULL
  */
-
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	int d;
+	size_t height = 0;
+	size_t nodes = 0;
+	size_t power = 0;
 
-	if (tree == NULL)
+	if (!tree)
 		return (0);
 
-	d = binary_tree_depth(tree);
+	if (!tree->right && !tree->left)
+		return (1);
 
-	return  (binary_tree_perfect(tree, d));
+	height = binary_tree_height(tree);
+	nodes = binary_tree_size(tree);
+
+	power = (size_t)_pow_recursion(2, height + 1);
+	return (power - 1 == nodes);
+}
+
+/**
+ *_pow_recursion - returns the value of x raised to the power of y
+ *@x: the value to exponentiate
+ *@y: the power to raise x to
+ *Return: x to the power of y, or -1 if y is negative
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	else
+		return (x * _pow_recursion(x, y - 1));
+
+}
+
+/**
+ * binary_tree_size - measures the size of a binary tree
+ * @tree: tree to measure the size of
+ *
+ * Return: size of the tree
+ *         0 if tree is NULL
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+
+	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
+}
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: tree to measure the height of
+ *
+ * Return: height of the tree
+ *         0 if tree is NULL
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t height_l = 0;
+	size_t height_r = 0;
+
+	if (!tree)
+		return (0);
+
+	height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	return (height_l > height_r ? height_l : height_r);
 }
--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -3,34 +3,18 @@
 /**
  * binary_tree_sibling - finds the sibling of a node
  * @node: pointer to the node to find the sibling
- * Return: return a pointer to the sibling node
+ *
+ * Return: pointer to the sibling node
+ *         NULL if node is NULL
+ *         NULL if the parent is NULL
+ *         NULL if the node has no siblings
  */
-
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	binary_tree_t *temp = NULL;
-
-	if (node == NULL)
+	if (!node || !node->parent)
 		return (NULL);
 
-	if (node->parent == NULL)
-		return (NULL);
-
-	if (node->parent->left)
-	{
+	if (node == node->parent->left)
 		return (node->parent->right);
-	}
-
-	if (node->parent->right)
-	{
-		return (node->parent->left);
-	}
-
-	temp = binary_tree_sibling(node->parent->left);
-	if (temp != NULL)
-		return (temp);
-
-	temp = binary_tree_sibling(node->parent->right);
-
-	return (temp);
+	return (node->parent->left);
 }
